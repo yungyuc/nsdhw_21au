ad8bb14d82a1a14f5d4c55405ff31429e10e8b02
mylin0405 <bigearyoyoman@gmail.com>
2021-09-15 04:21:28 +0000

    first attempt of hw1

---
+++ hw1/mylin0405/countline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+argc=$#
+
+if [ $argc -lt 1 ]; then
+	echo "missing file name"
+elif [ $argc -gt 1 ]; then
+	echo "only one argument is allowed"
+else
+	file=$1
+	if [ -f $file ]; then
+		echo "$(cat $file | wc -l) lines in $file"
+	else
+		echo "$file not found"
+	fi
+fi
