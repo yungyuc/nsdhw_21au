ec1471df7fa4de6b4c3fbd50ee31fb464408fe0f
hcgcarry <hcgcarry@gmail.com>
2021-09-15 15:50:04 +0800

    finish hw1

---
+++ hw1/hcgcarry/countline.sh
@@ -0,0 +1,14 @@
+
+if [ $# -lt 1 ];then
+    echo "missing file name"
+elif [ $# -gt 1 ];then
+    echo "only one argument is allowed"
+else
+    if test -f "$1";then
+        line=$(cat $1 | wc -l)
+        echo "$line lines in $1"
+    else
+        echo "$1 not found"
+    fi
+
+fi
