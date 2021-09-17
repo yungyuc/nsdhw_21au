50823f3831d603cf7149591a6437598ba17923ea
secondlevel <qscesz201411@gmail.com>
2021-09-17 14:03:04 +0800

    fixed countline.sh

--- hw1/secondlevel/countline.sh
+++ hw1/secondlevel/countline.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-if (("$#" < "1")) 
-then
-	echo "missing file name"
-elif (("$#" > "1")) 
-then
-	echo "only one argument is allowed"
-else 
-	if [ -f "$1" ];
-	then
-		line_count=$(cat "$1" | wc -l)
-		echo  "$line_count lines in $1"
-	else
-		echo  "$1 not found"
-	fi	
+if [ "$#" -lt 1 ]; then
+  echo "missing file name"
+elif [ "$#" -gt 1 ]; then
+  echo "only one argument is allowed"
+else
+  if [ -f "$1" ]; then
+      linecount=$( cat "$1" | wc -l)
+      echo "${linecount} lines in $1"
+  else
+      echo "$1 not found"
+  fi
 fi
