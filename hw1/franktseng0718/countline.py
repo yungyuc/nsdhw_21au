b852bc61197472c642bb9ef14673a2f8dbca0438
franktseng0718 <franktseng0718@gmail.com>
2021-09-25 08:41:27 +0000

    first attempt

---
+++ hw1/franktseng0718/countline.py
@@ -0,0 +1,27 @@
+   
+#!/bin/bash
+
+import sys
+import os.path
+
+env = os.environ["PYTHON_BIN"]
+if env == "python2" or env == 'python3':
+    """:"
+    exec env $0 $@
+    ":"""
+else:
+    print("exec: {}: not found".format(env))
+    sys.exit(1)
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
