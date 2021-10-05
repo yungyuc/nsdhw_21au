8c40bae866763f01608daab437574e594d2d04ef
LittleLaGi <yam5421363@gmail.com>
2021-10-05 17:40:36 +0800

    Add more test for q2

--- hw2/LittleLaGi/q2/test_getAngle.py
+++ hw2/LittleLaGi/q2/test_getAngle.py
@@ -13,3 +13,19 @@ def test_2():
 def test_3():
     x = _vector.getAngle([111, 98, -12], [-11, 1000, -51.1])
     assert round(x, 2) == 0.86
+
+def test_4():
+    x = _vector.getAngle([0.0001, -0.0001], [0.0001, -0.0001])
+    assert round(x, 2) == 0.00
+
+def test_5():
+    x = _vector.getAngle([1000.11, 0.0005], [9999.99, 6666.66])
+    assert round(x, 2) == 0.59
+
+def test_6():
+    x = _vector.getAngle([1, -1], [-1, 1])
+    assert round(x, 2) == 3.14
+
+def test_7():
+    x = _vector.getAngle([12345, 12345], [24690, 24690])
+    assert round(x, 2) == 0.00
