2b7efe9e2e76499c73f57c9d49392efa0c356009
nctu16028 <410146liao@gmail.com>
2021-10-08 00:10:32 +0800

    Q2: add zero-length validation and safe acos

--- hw2/nctu16028/q2/vecAngle.cpp
+++ hw2/nctu16028/q2/vecAngle.cpp
@@ -7,8 +7,20 @@ float angle_between(float x1, float y1, float x2, float y2)
     dot_product = x1*x2 + y1*y2;
     norm1 = std::sqrt(x1*x1 + y1*y1);
     norm2 = std::sqrt(x2*x2 + y2*y2);
+
+    // If one of the vectors is of length 0, the input is invalid
+    if(norm1*norm2 == 0)
+        return -1;
+
     cos_val = dot_product / (norm1*norm2);
-    theta = std::acos(cos_val);
+    
+    // safe acos: to get rid of some problems caused by floating point precision
+    if(cos_val <= -1.0)
+        theta = std::acos(-1);
+    else if(cos_val >= 1.0)
+        theta = std::acos(1);
+    else
+        theta = std::acos(cos_val);
 
     return theta;
 }
