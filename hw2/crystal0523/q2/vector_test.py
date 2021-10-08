14446a9175a70f25233dc76f4b1502a3c97aeafe
crystal0523 <a870523tw@gmail.com>
2021-10-08 22:05:28 +0800

    debug

--- hw2/crystal0523/q2/vector_test.py
+++ hw2/crystal0523/q2/vector_test.py
@@ -5,6 +5,6 @@ def test_90Angle():
     angle = _vector(1, 0, 0, 1) * 180 / math.pi
     assert round(angle, 1) == 90
 
-def test_90Angle():
+def test_180Angle():
     angle = _vector(1, 0, -1, 0) * 180 / math.pi
     assert round(angle, 1) == 180
