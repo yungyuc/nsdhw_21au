5d22d13e15ccafaa731991eb394b11d6c5ece054
crystal0523 <a870523tw@gmail.com>
2021-10-08 21:06:40 +0800

    modified

--- hw2/crystal0523/q2/_vector.cpp
+++ hw2/crystal0523/q2/_vector.cpp
@@ -6,8 +6,8 @@ namespace py = pybind11;
 
 float CalRadian(float x1, float y1, float x2, float y2)
 {
-    float vec = (y2-y1)/(x2-x1);
-    float radian = atan(vec);
+    float dot_product = x1*x2+y1*y2, len1 = sqrt(x1*x1+y1*y1), len2 = sqrt(x2*x2+y2*y2);
+    float radian = acos(dot_product/(len1*len2));
     return radian;
 }
 
