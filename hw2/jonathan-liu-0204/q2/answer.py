0cf5eb0205ad6f7d977476692a5bc7cbb45d5d1c
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2021-10-11 00:45:36 +0800

    test27

--- hw2/jonathan-liu-0204/q2/answer.py
+++ hw2/jonathan-liu-0204/q2/answer.py
@@ -1,27 +1,28 @@
 from _vector import *
-import pytest
 import math
 
 def test_zero_length():
     vector0 = [0, 0]
     vector1 = [3, 8]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), -1)
+    assert result < 0
     
 def test_zero_angle():
     vector0 = [1, 1]
     vector1 = [5, 5]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), 0)
+    assert result < 0.001
 
 def test_right_angle(): #90
     vector0 = [0, 3]
     vector1 = [5, 0]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), math.pi/2)
+    minus = math.pi / 2
+    assert (result - minus) < 0.001
 
 def test_flat_angle(): #180
     vector0 = [6, 0]
     vector1 = [-4, 0]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), math.pi)
+    minus = math.pi
+    assert (result - minus) < 0.001
