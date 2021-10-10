97ab5a8da843a22f926441341879bfceb26c768e
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2021-10-10 23:25:45 +0800

    test14 fixing the operation of _vector.cpp

--- hw2/jonathan-liu-0204/q2/_vector.cpp
+++ hw2/jonathan-liu-0204/q2/_vector.cpp
@@ -8,7 +8,7 @@ double get_angle(double x1, double y1, double x2, double y2){
     double length1 = sqrt(x1*x1 + y1*y1);
     double length2 = sqrt(x2*x2 + y2*y2);
 
-    if(length1 == 0 | length2 == 0){
+    if(length1 == 0 || length2 == 0){
         return true;
     }
 
