3e2a19978e9d9c36016aa2032c7be9e02c66c2b1
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2021-10-10 21:35:30 +0800

    add hw2 files

---
+++ hw2/YI-CHENG-SHIH645/q1/q1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "Line.h"
+
+Line::Line(size_t size) : m_points(std::vector<Point>(size)) {}
+
+float & Line::x(size_t it) {
+    return m_points.at(it).x;
+}
+
+float & Line::y(size_t it) {
+    return m_points.at(it).y;
+}
+
+size_t Line::size() const {
+    return m_points.size();
+}
+
+int main(int, char**)
+{
+    Line line(3);
+    line.x(0) = 0; line.y(0) = 1;
+    line.x(1) = 1; line.y(1) = 3;
+    line.x(2) = 2; line.y(2) = 5;
+
+    Line line2(line);
+    line2.x(0) = 9;
+
+    std::cout << "line: number of points = " << line.size() << std::endl;
+    for (size_t it=0; it<line.size(); ++it)
+    {
+        std::cout << "point " << it << ":"
+                  << " x = " << line.x(it)
+                  << " y = " << line.y(it) << std::endl;
+    }
+
+    std::cout << "line2: number of points = " << line.size() << std::endl;
+    for (size_t it=0; it<line.size(); ++it)
+    {
+        std::cout << "point " << it << ":"
+                  << " x = " << line2.x(it)
+                  << " y = " << line2.y(it) << std::endl;
+    }
+
+    return 0;
+}
