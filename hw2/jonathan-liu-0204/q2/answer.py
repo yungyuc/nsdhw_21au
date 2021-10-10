2fab85218d770664b046710bed62f1b28f2461d0
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2021-10-10 22:29:41 +0800

    test7-including q2

---
+++ hw2/jonathan-liu-0204/q2/answer.py
@@ -0,0 +1,27 @@
+import _vector
+import math
+import pytest
+
+def test_zero_length():
+    vector0 = [0, 0]
+    vector1 = [3, 8]
+    with pytest.raises(ValueError):
+        _vector.get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
+    
+def test_zero_angle():
+    vector0 = [1, 1]
+    vector1 = [5, 5]
+    result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
+    assert math.isclose(result, 0)
+
+def test_right_angle(): #90
+    vector0 = [0, 3]
+    vector1 = [5, 0]
+    result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
+    assert math.isclose(result, math.pi/2)
+
+def test_flat_angle(): #180
+    vector0 = [6, 0]
+    vector1 = [-4, 0]
+    result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
+    assert math.isclose(result, math.pi)
---
+++ hw2/jonathan-liu-0204/q2/get_angle.cpp
@@ -0,0 +1,25 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <cmath>
+
+namespace py = pybind11;
+
+double get_angle(double x1, double y1, double x2, double y2){
+
+    double length1 = sqrt(x1*x1 + y1*y1);
+    double length2 = sqrt(x2*x2 + y2*y2);
+
+    if(length1 == 0 | length2 == 0){
+        return true;
+    }
+
+    double inner = x1*x2 + y1*y2;
+    double cosine = inner / (length1*length2);
+
+
+    return acos(cosine);
+}
+
+PYBIND11_MODULE(_vector, m){
+    m.def("The angle in radians is: ", &get_angle);
+}
