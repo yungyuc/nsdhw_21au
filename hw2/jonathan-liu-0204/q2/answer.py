6917f9fcbd117a87524e9ff1d604c68a8764b6d0
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2021-10-11 00:13:32 +0800

    test23

--- hw2/jonathan-liu-0204/q2/answer.py
+++ hw2/jonathan-liu-0204/q2/answer.py
@@ -1,27 +1,26 @@
 from _vector import *
 import math
-import pytest
 
 def test_zero_length():
     vector0 = [0, 0]
     vector1 = [3, 8]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(result, -1)
+    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), -1)
     
 def test_zero_angle():
     vector0 = [1, 1]
     vector1 = [5, 5]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(result, 0)
+    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), 0)
 
 def test_right_angle(): #90
     vector0 = [0, 3]
     vector1 = [5, 0]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(result, math.pi/2)
+    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), math.pi/2)
 
 def test_flat_angle(): #180
     vector0 = [6, 0]
     vector1 = [-4, 0]
     result = get_angle(vector0[0], vector0[1], vector1[0], vector1[1])
-    assert math.isclose(result, math.pi)
+    assert math.isclose(get_angle(vector0[0], vector0[1], vector1[0], vector1[1]), math.pi)
