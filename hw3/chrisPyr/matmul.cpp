e26e6e8e1f9b48b5ddba3e16862bcb882b23150b
chrisPyr <smile51293@gmail.com>
2021-10-25 11:34:32 +0800

    [HW3] hw3 init

---
+++ hw3/chrisPyr/matmul.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Mat.hpp"
+void NaiveMatMul(double *A_mat, double *B_mat, double *C_mat){
+
+  return ;
+}
+
+void TilingMatMul(double *A_mat, double *B_mat, double *C_mat)
+int main(){
+
+
+
+  return 0;
+}
