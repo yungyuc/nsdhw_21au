afdbc10ab3a7bffdf7beeba7a4767d0cce37c55d
chrisPyr <smile51293@gmail.com>
2021-10-27 17:09:26 +0800

    [HW3] re-format hw

--- hw3/chrisPyr/Mat.cpp
+++ hw3/chrisPyr/Mat.cpp
@@ -1,16 +1,33 @@
 #include "Mat.hpp"
 
-Matrix Matrix::NaiveMatmul(Matrix  *other){
-  Matrix res(this->getrow(), other->getcol());
-  for(size_t i=0; i<this->getrow(); ++i){
-    for(size_t j=0; j<other->getcol(); ++j){
+Matrix::Matrix(Matrix &other) {
+  this->m_row = other.m_row;
+  this->m_col = other.m_col;
+  for (size_t i = 0; i < m_row; ++i) {
+    for (size_t j = 0; j < m_col; ++j) {
+      this->getpos(i, j) = other.getdata(i, j);
+    }
+  }
+}
+
+Matrix &Matrix::operator=(Matrix &&other) {
+
+  std::swap(m_row, other.m_row);
+  std::swap(m_col, other.m_col);
+  std::swap(data, other.data);
+  return *this;
+}
+
+Matrix multiply_naive(Matrix &A_mat, Matrix &B_mat) {
+  Matrix res(A_mat.getrow(), B_mat.getcol());
+  for (size_t i = 0; i < A_mat.getrow(); ++i) {
+    for (size_t j = 0; j < B_mat.getcol(); ++j) {
       double result = 0;
-      for(size_t k=0; k<this->getcol(); ++k){
-        result += this->getdata(i,k) * other->getdata(k,j);
+      for (size_t k = 0; k < A_mat.getcol(); ++k) {
+        result += A_mat.getdata(i, k) * B_mat.getdata(k, j);
       }
-      res.getpos(i,j) = result;
+      res.getpos(i, j) = result;
     }
   }
   return res;
 }
-
--- hw3/chrisPyr/main.cpp
+++ hw3/chrisPyr/main.cpp
@@ -1,31 +1,24 @@
-#include <iostream>
 #include "Mat.hpp"
+#include <iostream>
 
-
-int main(){
+int main() {
   using namespace std;
-  Matrix A(10,20);
-  Matrix B(20,30);
+  Matrix A(10, 20);
+  Matrix B(20, 30);
 
-  for(int i=0; i<10; ++i){
-    for(int j=0; j<20; ++j){
-      A.getpos(i,j) = 5;
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 20; ++j) {
+      A.getpos(i, j) = 5;
     }
   }
 
-  for(int i=0; i<20; ++i){
-    for(int j=0; j<30; ++j){
-      B.getpos(i,j) = 5;
+  for (int i = 0; i < 20; ++i) {
+    for (int j = 0; j < 30; ++j) {
+      B.getpos(i, j) = 5;
     }
   }
 
-  Matrix C = A.NaiveMatmul(&B);
-  cout<<C<<endl;
-  //for(int i=0; i<10;++i){
-  //  for(int j=0; j<30; ++j){
-  //    cout<<C.getdata(i,j)<<" ";
-  //  }
-  //  cout<<endl;
-  //}
+  Matrix C = multiply_naive(A, B);
+  cout << C << endl;
   return 0;
 }
