725d74f96d18dd33b425f0dd4e59ddc08c8f8725
Hsiang-Chieh Tsou <hsjts0u@gmail.com>
2021-10-27 20:41:19 +0800

    cpp files

---
+++ hw3/hsjts0u/_matrix.hpp
@@ -0,0 +1,37 @@
+#include <cstddef>
+#include <algorithm>
+#include <mkl.h>
+
+class Matrix 
+{
+public:
+    
+    Matrix(size_t nrow, size_t ncol): m_nrow(nrow), m_ncol(ncol)
+    {
+        size_t nelement = nrow * ncol;
+        m_buffer = new double[nelement]();
+    }
+    
+    ~Matrix()
+    {
+        delete [] m_buffer;
+    }
+
+    
+    double   operator() (size_t row, size_t col) const { return m_buffer[row*m_ncol + col]; }
+    double & operator() (size_t row, size_t col)       { return m_buffer[row*m_ncol + col]; }
+    double * get_buffer() const { return m_buffer; }
+    size_t nrow() const { return m_nrow; }
+    size_t ncol() const { return m_ncol; }
+
+private:
+    
+    size_t m_nrow;
+    size_t m_ncol;
+    double *m_buffer;
+
+};
+
+Matrix multiply_naive(Matrix const & mat1, Matrix const & mat2);
+Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2, size_t tsize);
+Matrix& multiply_mkl(Matrix const & mat1, Matrix const & mat2);
