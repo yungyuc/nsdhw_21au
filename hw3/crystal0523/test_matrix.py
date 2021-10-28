75802c736bd18aa5542d80fad6b994ce0fe1866d
crystal0523 <a870523tw@gmail.com>
2021-10-28 23:21:39 +0800

    modified

--- hw3/crystal0523/test_matrix.py
+++ hw3/crystal0523/test_matrix.py
@@ -5,12 +5,13 @@ import sys
 import os
 import pytest
 
-class Test_matrix(unittest.TestCase):
+class Test_GEMM:
 
     def make_matrices(self, size):
-        mat1 = _matrix.Matrix(size,size)
-        mat2 = _matrix.Matrix(size,size)
-        mat3 = _matrix.Matrix(size,size)
+
+        mat1 = Matrix(size,size)
+        mat2 = Matrix(size,size)
+        mat3 = Matrix(size,size)
 
         for it in range(size):
             for jt in range(size):
@@ -20,66 +21,70 @@ class Test_matrix(unittest.TestCase):
 
         return mat1, mat2, mat3
 
-
     def test_basic(self):
+
         size = 100
         mat1, mat2, mat3, *_ = self.make_matrices(size)
 
-        assert size == mat1.nrow()
-        assert size == mat1.ncol()
-        assert size == mat2.nrow()
-        assert size == mat2.ncol()
-        assert size == mat3.nrow()
-        assert size == mat3.ncol()
-       
+        assert size == mat1.nrow
+        assert size == mat1.ncol
+        assert size == mat2.nrow
+        assert size == mat2.ncol
+        assert size == mat3.nrow
+        assert size == mat3.ncol
+
         assert 2 == mat1[0,1]
         assert size+2 == mat1[1,1]
         assert size*2 == mat1[1,size-1]
         assert size*size == mat1[size-1,size-1]
-      
-        for i in range(mat1.nrow()):
-            for j in range(mat1.ncol()):
+
+        for i in range(mat1.nrow):
+            for j in range(mat1.ncol):
                 assert 0 != mat1[i,j]
                 assert mat1[i,j] == mat2[i,j]
                 assert 0 == mat3[i,j]
+
         assert mat1 == mat2
         assert mat1 is not mat2
-       
+
     def test_match_naive_mkl(self):
+
         size = 100
         mat1, mat2, *_ = self.make_matrices(size)
 
         ret_naive = _matrix.multiply_naive(mat1, mat2)
         ret_mkl = _matrix.multiply_mkl(mat1, mat2)
 
-        assert size == ret_naive.nrow()
-        assert size == ret_naive.ncol()
-        assert size == ret_mkl.nrow()
-        assert size == ret_mkl.ncol()
+        assert size == ret_naive.nrow
+        assert size == ret_naive.ncol
+        assert size == ret_mkl.nrow
+        assert size == ret_mkl.ncol
 
-        for i in range(ret_naive.nrow()):
-            for j in range(ret_naive.ncol()):
+        for i in range(ret_naive.nrow):
+            for j in range(ret_naive.ncol):
                 assert mat1[i,j] is not ret_mkl[i,j]
                 assert ret_naive[i,j] == ret_mkl[i,j]
 
     def test_zero(self):
+
         size = 100
         mat1, mat2, mat3, *_ = self.make_matrices(size)
 
         ret_naive = _matrix.multiply_naive(mat1, mat3)
         ret_mkl = _matrix.multiply_mkl(mat1, mat3)
 
-        assert size == ret_naive.nrow()
-        assert size == ret_naive.ncol()
-        assert size == ret_mkl.nrow()
-        assert size == ret_mkl.ncol()
+        assert size == ret_naive.nrow
+        assert size == ret_naive.ncol
+        assert size == ret_mkl.nrow
+        assert size == ret_mkl.ncol
 
-        for i in range(ret_naive.nrow()):
-            for j in range(ret_naive.ncol()):
+        for i in range(ret_naive.nrow):
+            for j in range(ret_naive.ncol):
                 assert 0 == ret_naive[i,j]
                 assert 0 == ret_mkl[i,j]
 
     def check_tile(self, mat1, mat2, tsize):
+
         if 0 == tsize:
             ret_tile = _matrix.multiply_naive(mat1, mat2)
             tile_str = "_matrix.multiply_naive(mat1, mat2)"
@@ -88,8 +93,8 @@ class Test_matrix(unittest.TestCase):
             tile_str = "_matrix.multiply_tile(mat1, mat2, tsize)"
         ret_mkl = _matrix.multiply_mkl(mat1, mat2)
 
-        for i in range(ret_tile.nrow()):
-            for j in range(ret_tile.ncol()):
+        for i in range(ret_tile.nrow):
+            for j in range(ret_tile.ncol):
                 assert mat1[i,j] is not ret_mkl[i,j]
                 assert ret_tile[i,j] == ret_mkl[i,j]
 
@@ -130,5 +135,3 @@ class Test_matrix(unittest.TestCase):
             print("tile 19 ratio:", ratio19)
             print("time19/time0:", time19/time0)
         assert ratio19/ratio0 < 0.8
-     
-
