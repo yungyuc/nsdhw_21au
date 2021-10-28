9136ccc14b6fb5c73f11b0bc44ca21720f056287
Hydr8O <bananov888@gmail.com>
2021-10-29 02:13:16 +0800

    fix naming

--- hw3/Hydr8O/check_performance.py
+++ hw3/Hydr8O/check_performance.py
@@ -10,4 +10,4 @@ ret_tile = _matrix.multiply_tile(mat1, mat2, 64)
 with open('performance.txt', 'w') as performance:
     performance.write(f'Naive time: {ret_naive.elapsed()}\n')
     performance.write(f'Tiling time: {ret_tile.elapsed()}\n')
-    performance.write(f'Speed up: {ret_native.elapsed() / ret_tile.elapsed()}\n')
+    performance.write(f'Speed up: {ret_naive.elapsed() / ret_tile.elapsed()}\n')
