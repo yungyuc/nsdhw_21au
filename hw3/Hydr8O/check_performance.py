7f8492c0c31cfd6875ceeffb46ab4a080ff966e8
Hydr8O <bananov888@gmail.com>
2021-10-29 12:00:38 +0800

    change tile size

--- hw3/Hydr8O/check_performance.py
+++ hw3/Hydr8O/check_performance.py
@@ -5,7 +5,7 @@ size = 2000
 mat1 = _matrix.Matrix(size,size)
 mat2 = _matrix.Matrix(size,size)
 ret_naive = _matrix.multiply_naive(mat1, mat2)
-ret_tile = _matrix.multiply_tile(mat1, mat2, 64)
+ret_tile = _matrix.multiply_tile(mat1, mat2, 16)
 
 with open('performance.txt', 'w') as performance:
     performance.write(f'Naive time: {ret_naive.elapsed()}\n')
