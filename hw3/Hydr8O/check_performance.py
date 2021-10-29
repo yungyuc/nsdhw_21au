2c5fa215a62ae77eb4a3ac55af2aaa1c6964660d
Hydr8O <bananov888@gmail.com>
2021-10-29 12:12:53 +0800

    use timeit

--- hw3/Hydr8O/check_performance.py
+++ hw3/Hydr8O/check_performance.py
@@ -1,13 +1,21 @@
 import _matrix
+import timeit
 
-size = 2000
+size = 1000
 
 mat1 = _matrix.Matrix(size,size)
 mat2 = _matrix.Matrix(size,size)
-ret_naive = _matrix.multiply_naive(mat1, mat2)
-ret_tile = _matrix.multiply_tile(mat1, mat2, 16)
+
+ns = dict(_matrix=_matrix, mat1=mat1, mat2=mat2)
+t_tile = timeit.Timer("_matrix.multiply_tile(mat1, mat2, 16)", globals=ns)
+t_naive = timeit.Timer('_matrix.multiply_naive(mat1, mat2)', globals=ns)
+
+
+time_tile = min(t_tile.repeat(10, 1))
+time_naive = min(t_naive.repeat(10, 1))
+ratio = time_tile/time_naive
 
 with open('performance.txt', 'w') as performance:
-    performance.write(f'Naive time: {ret_naive.elapsed()}\n')
-    performance.write(f'Tiling time: {ret_tile.elapsed()}\n')
-    performance.write(f'Speed up: {ret_naive.elapsed() / ret_tile.elapsed()}\n')
+    performance.write(f'Naive time: {time_naive.elapsed()}\n')
+    performance.write(f'Tiling time: {time_tile.elapsed()}\n')
+    performance.write(f'Speed up: {ratio}\n')
