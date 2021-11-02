610f205aaf03456e6ad9d9d46cd7046d6744a03e
jack <gmail.com>
2021-11-02 13:51:35 +0800

    test

--- hw3/09510/test_matrix.py
+++ hw3/09510/test_matrix.py
@@ -10,49 +10,59 @@ import _matrix
 #=======================================================================
 class TestMatrix:
 
-    def calculate(self, p, q, r, tsize):
-        _m1 = np.random.rand(p, q)
-        _m2 = np.random.rand(q, r)
-        np_m = np.matmul(_m1, _m2)
+    def multiplier(self,row,col1,col2,tsize_list):
+        _1d_m1 = np.random.rand(row*col1)
+        _1d_m2 = np.random.rand(col1*col2)
 
-        m1 = _matrix.Matrix(_m1)
-        m2 = _matrix.Matrix(_m2)
+        _2d_m1 = _1d_m1.reshape(row,col1)
+        _2d_m2 = _1d_m2.reshape(col1,col2)
+        np_ret = np.matmul(_2d_m1,_2d_m2)
 
-        start = time.perf_counter()
-        ret_naive = _matrix.multiply_naive(m1, m2)
-        naive = time.perf_counter() - start
+        m1 = _matrix.Matrix(row,col1,_1d_m1)
+        m2 = _matrix.Matrix(col1,col2,_1d_m2)
 
-        start = time.perf_counter()
-        ret_tile = _matrix.multiply_tile(m1, m2, tsize)
-        tile = time.perf_counter() - start
+        #naive multiply
+        time_start = time.time()
+        naive_ret = _matrix.multiply_naive(m1,m2)
+        time_end = time.time()
+        naive_time = time_end - time_start
 
-        start = time.perf_counter()
-        ret_mkl = _matrix.multiply_mkl(m1, m2)
-        mkl = time.perf_counter() - start
+        #MKL multiply
+        time_start = time.time()
+        mkl_ret = _matrix.multiply_mkl(m1,m2)
+        time_end = time.time()
+        mkl_time = time_end - time_start
 
-        for i in range(p):
-            for j in range(r):
-                assert np_m[i, j] == approx(ret_naive[i, j])
-                assert np_m[i, j] == approx(ret_tile[i, j])
-                assert np_m[i, j] == approx(ret_mkl[i, j])
+        #tiling multipy
+        tile_time = []
+        tile_ret = []
+        for i in range(len(tsize_list)):
+            time_start = time.time()
+            tile_ret.append(_matrix.multiply_tile(m1,m2,tsize_list[i]))
+            time_end = time.time()
+            tile_time.append(time_end - time_start)
 
-        return naive, tile, mkl
+        for i in range(row):
+            for j in range(col2):
+                assert np_ret[i][j] == pytest.approx(naive_ret[i,j],rel = 1e-6)
+                assert np_ret[i][j] == pytest.approx(mkl_ret[i,j],rel = 1e-6)
+                for k in range(len(tsize_list)):
+                    assert np_ret[i][j] == pytest.approx(tile_ret[k][i,j],rel = 1e-6)
+        return naive_time,mkl_time,tile_time
 
-    def write_file(self, naive, tile, mkl):
-        with open('performance.txt', 'w') as f:
-            f.write('naive time: '+ str(naive) + " seconds\n")
-            f.write('tile: '+ str(tile) + " seconds\n")
-            f.write('mkl: '+ str(mkl) + " seconds\n")
-            f.write('tile speeds up compared to naive: '+ str(naive / tile) + " times\n")
-            f.write('MKL speeds up compared to naive: '+ str(naive / mkl) + " times\n")
 
-    def test_1000(self):
-        naive_min, tile_min, mkl_min = self.calculate(1000, 1000, 1000, 2)
-        for i in range(2,6):
-            naive, tile, mkl = self.calculate(1000, 1000, 1000, 2**i)
-            naive_min = min(naive, naive_min)
-            tile_min = min(tile, tile_min)
-            mkl_min = min(mkl, mkl_min)
-        self.write_file(naive_min, tile_min, mkl_min)
+    def write_file(self,file_name,naive_t,mkl_t,tile_t,tile_size):
+        with open(file_name,'w') as f:
+            f.write("|"+"method".center(16,"-")+"|"+"time(s)".center(16,"-")+"|"+"speedup rate".center(16,"-")+"|\n")
+            f.write("|"+'naive'.center(16)+"|"+str(round(naive_t,2)).center(16) +"|"+"1".center(16)+"|\n")
+            f.write("|"+"mkl".center(16)+"|"+str(round(mkl_t,2)).center(16)+"|"+str(round(naive_t/mkl_t,2)).center(16)+"|\n")
+            for i in range(len(tile_size)):
+                s = 'tiling('+str(tile_size[i])+"x"+str(tile_size[i])+")"
+                f.write("|"+s.center(16)+"|"+str(round(tile_t[i],2)).center(16) + "|" + str(round(naive_t/tile_t[i],2)).center(16)+"|\n")
+
+    def test_matrix(self):
+        tile_size = [2,4,8,16,32]
+        n_t,m_t,t_t = self.multiplier(1000,1000,1000,tile_size)
+        self.write_file("performance.txt",n_t,m_t,t_t,tile_size)
 #======================================================================
 
