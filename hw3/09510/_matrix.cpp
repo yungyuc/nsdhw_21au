9fde7288f13ad38df5b7453aad976ae5cdb2f242
jack <gmail.com>
2021-11-02 14:29:55 +0800

    tile fix

--- hw3/09510/_matrix.cpp
+++ hw3/09510/_matrix.cpp
@@ -26,41 +26,34 @@ Matrix multiply_naive(Matrix const & a, Matrix const & b)
 
 }
 
-Matrix multiply_tile(Matrix const & a, Matrix const & b, size_t tile)
+Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2, size_t tsize)
 {
-    if (a.ncol() != b.nrow()) {
-        throw out_of_range(
-            "the number of first matrix column "
-            "differs from that of second matrix row");
-    }
-    size_t bound_i, bound_j, bound_k;
-    Matrix ans(a.nrow(), b.ncol());
 
-    for (size_t i = 0; i < a.nrow(); i += tile)
+    Matrix ret(mat1.nrow(), mat2.ncol());
+
+    for (size_t row = 0; row < mat1.nrow(); row += tsize) 
     {
-        for (size_t j = 0; j < b.ncol(); j += tile) 
+        for (size_t col = 0; col < mat2.ncol(); col += tsize) 
         {
-            bound_i = std::min(a.nrow(), i + tile);
-            bound_j = std::min(b.ncol(), j + tile);
-            for (size_t k = 0; k < a.ncol(); k += tile)
+            for (size_t inner = 0; inner < mat1.ncol(); inner += tsize) 
             {
-                bound_k = std::min(a.ncol(), k + tile);
-
-                for (size_t t_k = k; t_k < bound_k; t_k++)
+                // operation for tile
+                for (size_t k = inner; k < std::min(mat1.ncol(), inner + tsize); k++) 
                 {
-                    for (size_t t_i = i; t_i < bound_i; t_i++)
+                    for (size_t i = row; i < std::min(mat1.nrow(), row + tsize); i++) 
                     {
-                        for (size_t t_j = j; t_j < bound_j; t_j++)
-                        {
-                            ans(t_i, t_j) += a(t_i, t_k) * b(t_k, t_j);
+                        for (size_t j = col; j < std::min(mat2.ncol(), col + tsize); j++) {
+                            ret(i, j) += mat1(i, k) * mat2(k, j);
                         }
                     }
                 }
             }
         }
     }
-    return ans;
 
+    return ret;
+
+}
 }
 
 Matrix multiply_mkl(Matrix const & a, Matrix const & b)
