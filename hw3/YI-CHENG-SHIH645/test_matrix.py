fb1a6088a8e847f24138572bb1c77dad8f80a798
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2021-11-04 13:48:24 +0800

    add hw3 files

--- hw3/YI-CHENG-SHIH645/test_matrix.py
+++ hw3/YI-CHENG-SHIH645/test_matrix.py
@@ -1,35 +1,35 @@
-import matrix
+import _matrix
 import time
 
 
 def test_correctness():
-    mat1 = matrix.Matrix(1024, 1024)
-    mat2 = matrix.Matrix(1024, 1024)
-    matrix.initialize(mat1)
+    mat1 = _matrix.Matrix(1024, 1024)
+    mat2 = _matrix.Matrix(1024, 1024)
+    _matrix.initialize(mat1)
     mat2 = mat1
-    naive_ret = matrix.multiply_naive(mat1, mat2)
-    tile_ret = matrix.multiply_tile(mat1, mat2, 64)
-    mkl_ret = matrix.multiply_mkl(mat1, mat2)
+    naive_ret = _matrix.multiply_naive(mat1, mat2)
+    tile_ret = _matrix.multiply_tile(mat1, mat2, 64)
+    mkl_ret = _matrix.multiply_mkl(mat1, mat2)
     assert naive_ret == tile_ret
     assert tile_ret == mkl_ret
 
 
 def test_performance():
-    mat1 = matrix.Matrix(1024, 1024)
-    mat2 = matrix.Matrix(1024, 1024)
-    matrix.initialize(mat1)
+    mat1 = _matrix.Matrix(1024, 1024)
+    mat2 = _matrix.Matrix(1024, 1024)
+    _matrix.initialize(mat1)
     mat2 = mat1
 
     start = time.time()
-    _ = matrix.multiply_naive(mat1, mat2)
+    _ = _matrix.multiply_naive(mat1, mat2)
     naive_time = time.time()-start
 
     start = time.time()
-    _ = matrix.multiply_tile(mat1, mat2, 64)
+    _ = _matrix.multiply_tile(mat1, mat2, 64)
     tile_time = time.time()-start
 
     start = time.time()
-    _ = matrix.multiply_mkl(mat1, mat2)
+    _ = _matrix.multiply_mkl(mat1, mat2)
     mkl_time = time.time()-start
 
     lines = [f'naive_time: {naive_time:.6f}\n',
