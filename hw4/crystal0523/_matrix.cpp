41c0516d3909331bea00c146405af9d377414343
crystal0523 <a870523tw@gmail.com>
2021-11-22 11:01:09 +0800

    modified

--- hw4/crystal0523/_matrix.cpp
+++ hw4/crystal0523/_matrix.cpp
@@ -21,6 +21,10 @@ public:
     {
         buffer_.reserve(size());
     }
+    ~Matrix()
+    {
+        reset_buffer(0,0);
+    }
     Matrix & operator=(Matrix const & other);
     bool operator== (Matrix const & mat1)const;
     double   operator() (size_t row, size_t col) const { return buffer_[index(row, col)]; }
@@ -29,11 +33,18 @@ public:
     size_t ncol() const { return col_; }
     size_t size() const { return row_ * col_; }
     size_t index(size_t row, size_t col) const{ return row * col_ + col; }
-    
+    void reset_buffer(size_t nrow, size_t ncol)
+    {
+        const size_t nelement = nrow * ncol;
+        buffer_.resize(nelement, 0);
+        row_ = nrow;
+        col_ = ncol;
+    }
 
 private:
     size_t row_ = 0;
     size_t col_ = 0;
+    
     
 };
 
@@ -44,6 +55,11 @@ size_t bytes() { return alloc.counter.bytes(); };
 //error
 Matrix & Matrix::operator=(Matrix const & other)
 {
+    if (this == &other) { return *this; }
+    if (row_ != other.row_ || col_ != other.col_)
+    {
+        reset_buffer(other.row_, other.col_);
+    }
     for(int i=0; i<other.row_ ;i++)
     {
         for(int j=0;j<other.col_; j++)
@@ -174,6 +190,7 @@ Matrix multiply_mkl(Matrix const & mat1, Matrix const & mat2)
 }
 
 
+
 PYBIND11_MODULE(_matrix, m) {
     m.def("multiply_naive", &multiply_naive);
     m.def("multiply_tile", &multiply_tile);
@@ -189,6 +206,7 @@ PYBIND11_MODULE(_matrix, m) {
         .def("__setitem__", [](Matrix &m, std::pair<size_t, size_t>i, const double v){
             return m(i.first, i.second)=v;
         }, py::is_operator())     
+        .def("reset_buffer", &Matrix::reset_buffer)
         .def("assign", &Matrix::operator=)
         .def(py::self==py::self)   
         .def_property_readonly("nrow", &Matrix::nrow)
